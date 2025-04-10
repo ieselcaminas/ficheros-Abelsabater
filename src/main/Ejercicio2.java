9d5c1cfec4ffa0d5d8cf427d966be5b9c3bd13ec
Abelsabater <abesabmun@alu.edu.gva.es>
2025-04-10 14:10:40 +0200

    FicherosAbel

--- src/main/Ejercicio2.java
+++ src/main/Ejercicio2.java
@@ -3,7 +3,6 @@ import java.text.DateFormat;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Scanner;
-
 public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
